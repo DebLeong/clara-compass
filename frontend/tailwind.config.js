58bf116fb4d90dedcf56b934d33a23378c353335
Deborah Leong <37959355+DebLeong@users.noreply.github.com>
2025-10-04 18:54:56 +0800

    chore: add .gitignore and remove node_modules/build artifacts

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}",   // make sure this is correct
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
